b3b9f74fc85f975d381c3e1eb0fd772409c0003e
Rasmus Hauschild <rasmushauschild@hotmail.com>
2026-05-02 18:24:04 +0200

    Initial Commit

---
+++ sweep/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sweep
+//
+//  Created by Rasmus Hauschild on 02/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sweep/sweepApp.swift
@@ -0,0 +1,17 @@
+//
+//  sweepApp.swift
+//  sweep
+//
+//  Created by Rasmus Hauschild on 02/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct sweepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
